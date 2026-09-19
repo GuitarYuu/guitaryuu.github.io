f80bec268b5c1a3d5c5ad6fcffcf47d0660e06cf
GuitarYuu <118648188+GuitarYuu@users.noreply.github.com>
2026-09-20 07:46:47 +0800

    个人化配置：标题/域名/GitHub Pages 部署工作流

--- src/site.config.ts
+++ src/site.config.ts
@@ -42,7 +42,7 @@ export const theme: ThemeUserConfig = {
   /** Configure the header of your site. */
   header: {
     menu: [
-      { title: 'Blog', link: '/blog' },
+      { title: 'Blog', link: '/' },
       { title: 'Academic', link: '/academic' },
       { title: 'Projects', link: '/projects' },
       { title: 'Links', link: '/links' },
